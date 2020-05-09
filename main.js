11256533ff5ff5953e4c3022696dca84d19da0d4
bookere90 <eric90booker@gmail.com>
2020-05-09 00:34:25 -0700

    updated manifest with jquery content script and url matcher | additionally added jquery.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function getEnvironment(){
+$(document).ready(function(){
+    function getEnvironment(){
+        let tabUrl = window.location.toString();
+        alert(tabUrl);
+    }
 
-}
+    $("#body").load(function() {
+        getEnvironment();
+    });
+
+});
