8413e64990fa99ac206902a0ef6cc289fe6482bf
bookere90 <eric90booker@gmail.com>
2020-05-14 22:32:47 -0700

    error message if the app is not opened in a D365 environment

--- main.js
+++ main.js
@@ -6,6 +6,32 @@ $(document).ready(function(){;
         $("#mainFrame").attr("src", "querypage.html")
     })
 
+    function formatUrl(tabUrl){
+        //     // this function needs to get the environment URL and appends 'api/data/v9.1' to it
+        //     let tabUrl = getTabUrl;
+            // ensure that the url is a Dynamics environment
+            let regex = /https:\/\/.*\.crm\.dynamics\.com.*/;
+        
+            if(regex.test(tabUrl)){
+                // set regex1 to match only the envName.crm.dynamics.com portion of the URL
+                let regex1 = /https:\/\/.*\.crm\.dynamics\.com/;
+                // remove everything after the regex1 matched portion
+                let subString = tabUrl.split(regex1);
+                // go back to original URL and remove everything after dynamics.com
+                let formattedUrl = tabUrl.replace(subString[1], "");
+                // take original environment domain and append API endpoint to it
+                apiEndpoint = formattedUrl + "/api/data/v9.1";
+                // store this in local storage for later use
+                chrome.storage.local.set({"apiEndpoint": apiEndpoint}, function(){
+                    console.log("apiEndpoint set to ", apiEndpoint);
+                });
+            }
+            else{
+                $("body").html("<h6 id='errorMessage'>This extension must be used in the context of a Dynamics Environment</h6>").css({"background-color":"red"}).css({"text-align":"center"});
+                $("#errorMessage").css({"padding-top": "35%"});
+            }
+        }
+
 });
 
 function initializer(callback){
@@ -17,28 +43,5 @@ function initializer(callback){
     });
 }
 
-function formatUrl(tabUrl){
-//     // this function needs to get the environment URL and appends 'api/data/v9.1' to it
-//     let tabUrl = getTabUrl;
-    // ensure that the url is a Dynamics environment
-    let regex = /https:\/\/.*\.crm\.dynamics\.com.*/;
 
-    if(regex.test(tabUrl)){
-        // set regex1 to match only the envName.crm.dynamics.com portion of the URL
-        let regex1 = /https:\/\/.*\.crm\.dynamics\.com/;
-        // remove everything after the regex1 matched portion
-        let subString = tabUrl.split(regex1);
-        // go back to original URL and remove everything after dynamics.com
-        let formattedUrl = tabUrl.replace(subString[1], "");
-        // take original environment domain and append API endpoint to it
-        apiEndpoint = formattedUrl + "/api/data/v9.1";
-        // store this in local storage for later use
-        chrome.storage.local.set({"apiEndpoint": apiEndpoint}, function(){
-            console.log("apiEndpoint set to ", apiEndpoint);
-        });
-    }
-    else{
-        console.log("This extension can only be used in a Dynamics environment.");
-    }
-}
 
